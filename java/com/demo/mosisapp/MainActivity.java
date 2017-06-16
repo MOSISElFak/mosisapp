19ef867378c9f11768c83642c7ee02f9838fa6c4
bubulecka <bubulecka@users.noreply.github.com>
2017-06-17 00:19:18 +0200

    added MapsActivity
    - added maps to project
    - added MapsActivity (fragment, from Templates)
    - changed SignOut (removed AuthUI mediator)

--- java/com/demo/mosisapp/MainActivity.java
+++ java/com/demo/mosisapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -16,6 +17,12 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -109,7 +116,7 @@ public class MainActivity extends AppCompatActivity
         //noinspection SimplifiableIfStatement
         if (id == R.id.sign_out_menu) {
             //sign out
-            AuthUI.getInstance().signOut(this);
+            mFirebaseAuth.signOut();
             return true;
         }
 
@@ -129,5 +136,4 @@ public class MainActivity extends AppCompatActivity
         super.onResume();
         mFirebaseAuth.addAuthStateListener(mAuthStateListener);
     }
-
 }
